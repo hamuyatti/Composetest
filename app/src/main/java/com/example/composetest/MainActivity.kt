be5a8e86c2a99d34b72b36acd2548867ab9a0c63
hamuyatti <syosatc@i>
2021-07-06 12:14:14 +0900

    副作用を起こさない方法（remember?）

--- app/src/main/java/com/example/composetest/MainActivity.kt
+++ app/src/main/java/com/example/composetest/MainActivity.kt
@@ -6,16 +6,23 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.material.Button
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,6 +60,8 @@ class MainActivity : ComponentActivity() {
                 Text("Davenport, California")
                 Text("December 2018")
             }
+            Greeting(name = "hei")
+            MyScreenContent()
         }
 
     }
@@ -60,6 +69,31 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun DefaultPreview(){
       NewsStory()
+    }
+    
+    @Composable
+    fun Greeting(name:String){
+        Surface(color = Color.Blue) {
+            Text(text = "Hello $name",modifier = Modifier.padding(24.dp))
+        }
+    }
+    @Composable
+    fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
+        Column {
+            for (name in names) {
+                Greeting(name = name)
+                Divider(color = Color.Black)
+            }
+            Divider(color = Color.Transparent, thickness = 32.dp)
+            Counter()
+        }
+    }
+    @Composable
+    fun Counter() {
+        val count = remember { mutableStateOf(0) }
+        Button(onClick = {count.value++}) {
+            Text(text = "${count.value}")
+        }
     }
 }
 
