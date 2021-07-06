5d36be428a30c86e94c24cc749ba45671bbacd7e
hamuyatti <syosatc@i>
2021-07-06 14:21:42 +0900

    list

--- app/src/main/java/com/example/composetest/MainActivity.kt
+++ app/src/main/java/com/example/composetest/MainActivity.kt
@@ -1,101 +1,102 @@
 package com.example.composetest
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.material.Button
 
-class MainActivity : ComponentActivity() {
+
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            NewsStory()
-          }
-        }
-
-    @Composable
-    fun NewsStory() {
-        MaterialTheme{
-            val typography = MaterialTheme.typography
-            Column(
-                modifier = Modifier.padding(60.dp)
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.header),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .height(180.dp)
-                        .fillMaxWidth()
-                        .clip(shape = RoundedCornerShape(4.dp)),
-                    contentScale = ContentScale.Crop
-                )
-                Spacer(modifier = Modifier.height(80.dp))
-
-                Text("A day wandering through the sandhills " +
-                        "in Shark Fin Cove, and a few of the " +
-                        "sights I saw",
-                    overflow = TextOverflow.Ellipsis,
-                    style = typography.h6,
-                    maxLines = 2
-                )
-                Text("Davenport, California")
-                Text("December 2018")
+            MyApp {
+                MyScreenContent()
             }
-            Greeting(name = "hei")
-            MyScreenContent()
-        }
-
-    }
-    @Preview
-    @Composable
-    fun DefaultPreview(){
-      NewsStory()
-    }
-    
-    @Composable
-    fun Greeting(name:String){
-        Surface(color = Color.Blue) {
-            Text(text = "Hello $name",modifier = Modifier.padding(24.dp))
-        }
-    }
-    @Composable
-    fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
-        Column {
-            for (name in names) {
-                Greeting(name = name)
-                Divider(color = Color.Black)
-            }
-            Divider(color = Color.Transparent, thickness = 32.dp)
-            Counter()
-        }
-    }
-    @Composable
-    fun Counter() {
-        val count = remember { mutableStateOf(0) }
-        Button(onClick = {count.value++}) {
-            Text(text = "${count.value}")
         }
     }
 }
 
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+       MaterialTheme{
+        Surface(color = Color.Yellow) {
+            content()
+        }
+    }
+}
 
+@Composable
+fun MyScreenContent(names: List<String> = List(1000) { "Hello Android #$it" }) {
+    val counterState = remember { mutableStateOf(0) }
 
+    Column(modifier = Modifier.fillMaxWidth()) {
+        NameList(names, Modifier.weight(0.5f))
+        Counter(
+            count = counterState.value,
+            updateCount = { newCount ->
+                counterState.value = newCount
+            }
+        )
+    }
+}
+
+@Composable
+fun NameList(names: List<String>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(items = names) { name ->
+            Greeting(name = name)
+            Divider(color = Color.Black)
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    var isSelected by remember { mutableStateOf(false)}
+    val backgroundColor by animateColorAsState(if(isSelected) Color.Red else Color.Transparent )
+    Text(
+        text = "Hello $name!",
+        modifier = Modifier
+            .padding(24.dp)
+            .background(color = backgroundColor)
+            .clickable (onClick = { isSelected = !isSelected}))
+}
+
+@Composable
+fun Counter(count: Int, updateCount: (Int) -> Unit) {
+    Button(
+        onClick = { updateCount(count + 1) },
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = if (count > 7) Color.Green else Color.White
+        )
+    ) {
+        Text("I've been clicked $count times")
+    }
+}
+
+@Preview("MyScreen preview")
+@Composable
+fun DefaultPreview() {
+    MyApp {
+        MyScreenContent()
+    }
+}
